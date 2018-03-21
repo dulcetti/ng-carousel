d58282e966e33bbc6f5abc83b4970549db78055c
Bruno Dulcetti <bruno.dulcetti@wooza.com.br>
2018-03-21 20:14:33 -0300

    Initial configs e options * move the set of template on method of required bindings * create private method _mountOptions * mountOptions get the option binding and set to configure the carousel * put public variabels inside init * put the methods _verifyRequiredBindings and _mountOptions inside init

--- src/ng-carousel.controller.js
+++ src/ng-carousel.controller.js
@@ -14,15 +14,43 @@ export class NgCarouselComponentController {
 
     // 'private' methods
     _init() {
-        this.ngCarouselOptions.setOptions(this.options);
-        this.isReady = true;
-        this.template = this.template || 'default';
         this._verifyRequiredBindings();
+        this._mountOptions();
+
+        this.animType = null;
+        this.currentSlide = this.options.initialSlide;
+        this.isReady = true;
+        this.isTrackMoving = false;
+        this.nextOk = false;
+        this.prevOk = false;
+        this.slideStyle = {};
+        this.trackStyle = {};
+        this.transformType = null;
+        this.transitionType = null;
+        this.visibleIndicators = false;
+        this.visibleNext = this.options.visibleNext;
+        this.visiblePrev = this.options.visiblePrev;
+        this.width = 0;
+    }
+
+    _mountOptions() {
+        let options = angular.extend({}, this.ngCarouselOptions.getOptions());
+        this.options = this.options || {};
+
+        for (const item in options) {
+            if (options.hasOwnProperty(item) && this.options[item]) {
+                options[item] = this.options[item];
+            }
+        }
+
+        this.options = options;
     }
 
     _verifyRequiredBindings() {
         let i = 0,
             fieldsLength = this.requiredBindings.length;
+
+        this.template = this.template || 'default';
 
         for (i; i < fieldsLength; i++) {
             let field = this.requiredBindings[i];
@@ -34,7 +62,6 @@ export class NgCarouselComponentController {
         }
     }
 
-    // public methods
     $onInit() {
         this._init();
     }
